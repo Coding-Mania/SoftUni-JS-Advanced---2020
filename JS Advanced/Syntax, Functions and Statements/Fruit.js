15135ab408e2e73e793e678d6c6f39b5ba588ad2
CodingMaania <vasilmitovv@gmail.com>
2020-01-13 16:51:57 +0200

    Refactor

--- JS Advanced/Syntax, Functions and Statements/Fruit.js
+++ JS Advanced/Syntax, Functions and Statements/Fruit.js
@@ -1,8 +1,8 @@
 function solve(fruit, weight, price) {
-    let needetPrice = (weight * price) / 1000;
+    let neededPrice = (weight * price) / 1000;
     weight = weight / 1000;
 
-    let massege = `I need $${needetPrice.toFixed(2)} to buy ${weight.toFixed(2) } kilograms ${fruit}.`;
+    let massege = `I need $${neededPrice.toFixed(2)} to buy ${weight.toFixed(2) } kilograms ${fruit}.`;
 
     console.log(massege);
 }

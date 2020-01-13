f38ebaa133cdd90fcce298c135a371525df02c2f
CodingMaania <vasilmitovv@gmail.com>
2020-01-13 12:16:54 +0200

    Solve task

--- JS Advanced/Syntax, Functions and Statements/Fruit.js
+++ JS Advanced/Syntax, Functions and Statements/Fruit.js
@@ -1,3 +1,10 @@
-function solve() {
+function solve(fruit, weight, price) {
+    let needetPrice = (weight * price) / 1000;
+    weight = weight / 1000;
 
+    let massege = `I need $${needetPrice.toFixed(2)} to buy ${weight.toFixed(2) } kilograms ${fruit}.`;
+
+    console.log(massege);
 }
+
+solve('apple', 1563, 2.35);

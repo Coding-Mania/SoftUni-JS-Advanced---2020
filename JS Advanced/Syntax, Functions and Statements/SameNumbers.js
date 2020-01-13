82782783315505982477032ef84898f0dada5ea3
CodingMania <vasilmitovv@gmail.com>
2020-01-13 17:28:20 +0200

    Solve second task

---
+++ JS Advanced/Syntax, Functions and Statements/SameNumbers.js
@@ -0,0 +1,20 @@
+function solve(number) {
+
+    let numberAsString = number.toString();
+    let isSame = true;
+    let sum = +numberAsString[0];
+
+    for (let index = 1; index < numberAsString.length; index++) {
+
+        if (numberAsString[index] !== numberAsString[index - 1]) {
+            isSame = false;
+        }
+
+        sum += +numberAsString[index];
+    }
+
+    console.log(isSame);
+    console.log(sum);
+}
+
+solve(1234);

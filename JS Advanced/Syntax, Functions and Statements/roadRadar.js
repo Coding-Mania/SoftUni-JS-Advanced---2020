a99e8032aa449d9c345ecc0a3213715a4476eae6
CodingMania <vasilmitovv@gmail.com>
2020-01-14 14:24:19 +0200

    Solve task

---
+++ JS Advanced/Syntax, Functions and Statements/roadRadar.js
@@ -0,0 +1,19 @@
+function solve(input){
+
+   let [speed, area] = input;
+
+   let obj = {
+    'residential': 20,
+    'city': 50,
+    'interstate': 90,
+    'motorway': 130
+   }
+
+   if(speed - obj[area] > 0 && speed - obj[area] < 21){
+    console.log('speeding');
+   }else if(speed - obj[area] > 20 && speed - obj[area] < 41){
+    console.log('excessive speeding');
+   }else if(speed - obj[area] > 40){
+    console.log('reckless driving');
+   }
+}

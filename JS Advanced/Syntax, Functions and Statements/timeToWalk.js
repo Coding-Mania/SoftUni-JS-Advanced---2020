bbc8f960efb72d7c72be276bc39170be779da24c
CodingMania <vasilmitovv@gmail.com>
2020-01-14 17:43:16 +0200

    Solve task

---
+++ JS Advanced/Syntax, Functions and Statements/timeToWalk.js
@@ -0,0 +1,11 @@
+function solve(steps, stepLength, speed) {
+
+    let distance = steps * stepLength;
+    let breakTime = Math.floor(distance / 500) * 60;
+    let time = Math.round(distance / speed * 3.6 + breakTime);
+    let sec = time % 60;
+    let min = ((time - sec) / 60) % 60;
+    let hour = ((time - sec - min * 60) / 60) % 60;
+     
+    console.log(`${hour.toString().padStart(2, '0')}:${min.toString().padStart(2, '0')}:${sec.toString().padStart(2, '0')}`);
+}

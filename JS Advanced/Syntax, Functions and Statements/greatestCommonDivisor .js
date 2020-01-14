3e416c4c3510dd74af786f5be7b1c795ce09b264
CodingMania <vasilmitovv@gmail.com>
2020-01-14 17:52:55 +0200

    Solve task

---
+++ JS Advanced/Syntax, Functions and Statements/greatestCommonDivisor .js
@@ -0,0 +1,16 @@
+function solve(a, b){
+    let division = 0;
+
+    var lowerNumber = Math.min(a, b);
+
+    for (let index = 1; index <= lowerNumber; index++) {
+        const element = index;
+        
+        if(a % index === 0 && b % index === 0 )
+        {
+            division = index;
+        }
+    }
+
+    console.log(division);
+}

99f2084bfdf9779da348e93126f1efb3a46029e6
CodingMania <vasilmitovv@gmail.com>
2020-01-14 14:44:24 +0200

    Repair

--- JS Advanced/Syntax, Functions and Statements/Fruit.js
+++
@@ -1,8 +0,0 @@
-function solve(fruit, weight, price) {
-    let neededPrice = (weight * price) / 1000;
-    weight = weight / 1000;
-
-    let massage = `I need $${neededPrice.toFixed(2)} to buy ${weight.toFixed(2) } kilograms ${fruit}.`;
-
-    console.log(massage);
-}

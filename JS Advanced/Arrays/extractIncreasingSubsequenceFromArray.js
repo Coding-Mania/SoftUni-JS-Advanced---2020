b4b3332d74a625b4bd321514bff0b9beffcf6f16
CodingMania <vasilmitovv@gmail.com>
2020-01-17 17:23:49 +0200

    Solved tasks

--- JS Advanced/Arrays/extractIncreasingSubsequenceFromArray.js
+++ JS Advanced/Arrays/extractIncreasingSubsequenceFromArray.js
@@ -1,4 +1,18 @@
-function solve(arr){
-}
+function solve(arr) {
+    const subsequence = [];
 
-console.log(solve([1, 3, 8, 4, 10, 12, 3, 2, 24]));
+    let biggestNumber = arr.shift();
+    subsequence.push(biggestNumber);
+
+    for (let i = 0; i < arr.length; i++) {
+
+        const element = arr[i];
+
+        if (element >= biggestNumber) {
+            biggestNumber = element;
+            subsequence.push(biggestNumber);
+        }
+    }
+
+    return subsequence.join('\n');
+}

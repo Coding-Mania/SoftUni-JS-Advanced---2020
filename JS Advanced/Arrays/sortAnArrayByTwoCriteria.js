0bf3c60abb8f0fcdee73974ae1fec58130d7d8a4
CodingMania <vasilmitovv@gmail.com>
2020-01-17 17:29:35 +0200

    Code optimization

--- JS Advanced/Arrays/sortAnArrayByTwoCriteria.js
+++ JS Advanced/Arrays/sortAnArrayByTwoCriteria.js
@@ -1,11 +1,9 @@
 function solve(arr) {
-    const sortedArr = arr.sort((a, b) => {
+    return arr.sort((a, b) => {
         if (a.length - b.length === 0) {
             return a.localeCompare(b);
         } else {
             return a.length - b.length;
         }
-    });
-
-    return sortedArr.join('\n');
+    }).join('\n');
 }

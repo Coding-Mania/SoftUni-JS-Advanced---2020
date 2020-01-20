a854b72f6009500d96867ad95a47f656d85bfe58
CodingMania <vasilmitovv@gmail.com>
2020-01-20 17:13:53 +0200

    Code optimization

--- JS Advanced/Arrays/magicMatrices.js
+++ JS Advanced/Arrays/magicMatrices.js
@@ -8,16 +8,12 @@ function solve(matrix) {
             const element = matrix[rows][cols];
             rowsSum[rows] += element;
             colsSum[cols] += element;
+          
         }
-    }
 
-    for (let i = 1; i < rowsSum.length; i++) {
-        const element = rowsSum[i];
-        const element2 = colsSum[i];
-
-        if (element !== rowsSum[i - 1] || element2 !== colsSum[i - 1]) {
+        if ( rows !== 0 &&rowsSum[rows] !==  rowsSum[rows -1] &&  colsSum[rows] !==  colsSum[rows -1]) {
             return false;
-        }
+                    }
     }
 
     return true;

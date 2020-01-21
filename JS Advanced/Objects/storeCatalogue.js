1dabe4469cef4e3b2592a09fe4627bc1cffd1498
CodingMania <vasilmitovv@gmail.com>
2020-01-21 18:24:41 +0200

    Code refactoring

--- JS Advanced/Objects/storeCatalogue.js
+++ JS Advanced/Objects/storeCatalogue.js
@@ -7,9 +7,11 @@ function solve(input) {
     input.forEach(element => {
         let info = element.split(' : ');
         let obj = {};
+
         if (!store.hasOwnProperty(info[0][0])) {
             store[info[0][0]] = [];
         }
+
         obj[info[0]] = +info[1];
 
         store[info[0][0]].push(obj);

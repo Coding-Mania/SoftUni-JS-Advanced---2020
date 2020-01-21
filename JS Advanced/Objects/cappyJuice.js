40546cff0786f4c692f87aaae03ec676e867e9dc
CodingMania <vasilmitovv@gmail.com>
2020-01-21 16:46:46 +0200

    Solved task

---
+++ JS Advanced/Objects/cappyJuice.js
@@ -0,0 +1,41 @@
+function solve(input) {
+    const obj = {};
+    const botles = {};
+
+    input.forEach(element => {
+        let juice = element.split(' => ');
+
+        if (!obj.hasOwnProperty(juice[0])) {
+            obj[juice[0]] = 0;
+        }
+
+        obj[juice[0]] += +juice[1];
+
+        if (obj[juice[0]] >= 1000) {
+
+            let botlesCount = 0;
+
+            if (!botles.hasOwnProperty(juice[0])) {
+                botles[juice[0]] = 0;
+            }
+
+            while (obj[juice[0]] >= 1000) {
+                obj[juice[0]] -= 1000;
+                botlesCount++;
+            }
+
+            botles[juice[0]] += botlesCount;
+        }
+    });
+
+    for (const key in botles) {
+        console.log(`${key} => ${botles[key]}`);
+    }
+}
+
+solve(['Kiwi => 234',
+    'Pear => 2345',
+    'Watermelon => 3456',
+    'Kiwi => 4567',
+    'Pear => 5678',
+    'Watermelon => 6789']);

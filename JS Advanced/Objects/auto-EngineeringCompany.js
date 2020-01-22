8e10c6c817d89dff2a0e5c607b2f5e50793ae3e6
CodingMania <vasilmitovv@gmail.com>
2020-01-22 09:32:29 +0200

    Deleted unnecessary code

--- JS Advanced/Objects/auto-EngineeringCompany.js
+++ JS Advanced/Objects/auto-EngineeringCompany.js
@@ -34,16 +34,3 @@ function solve(input) {
         }
     }
 }
-
-solve([
-    'Mercedes-Benz | 50PS | 123',
-    'Mini | Clubman | 20000',
-    'Mini | Convertible | 1000',
-    'Mercedes-Benz | 60PS | 3000',
-    'Hyunday | Elantra GT | 20000',
-    'Mini | Countryman | 100',
-    'Mercedes-Benz | W210 | 100',
-    'Mini | Clubman | 1000',
-    'Mercedes-Benz | W163 | 200'
-]
-)

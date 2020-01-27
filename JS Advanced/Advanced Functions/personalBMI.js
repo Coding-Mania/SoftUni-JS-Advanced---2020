62b547984d23f7088df6e1e68308061cc9f0462b
CodingMania <vasilmitovv@gmail.com>
2020-01-27 15:05:25 +0200

    Deleted unnecessary code and refactoring

--- JS Advanced/Advanced Functions/personalBMI.js
+++ JS Advanced/Advanced Functions/personalBMI.js
@@ -1,7 +1,7 @@
 function solve(name, age, weight, height) {
 
-    let BMI =Math.round(weight / Math.pow((height / 100), 2));
-    let status = BMI < 18.5 ?	'underweight' :  BMI < 25 ? 'normal' : BMI < 30 ? 'overweight' : 'obese';
+    let BMI = Math.round(weight / (height / 100) ** 2);
+    let status = BMI < 18.5 ? 'underweight' : BMI < 25 ? 'normal' : BMI < 30 ? 'overweight' : 'obese';
 
 
     let personInfo = {
@@ -12,14 +12,12 @@ function solve(name, age, weight, height) {
             height
         },
         BMI,
-        status      
+        status
     }
 
     if (status === 'obese') {
-        Object.defineProperty(personInfo, 'recommendation', {value: 'admission required', enumerable: true});
+        Object.defineProperty(personInfo, 'recommendation', { value: 'admission required', enumerable: true });
     }
 
     return personInfo;
 }
-
-console.log(solve('Honey Boo Bo', 9, 57, 137));

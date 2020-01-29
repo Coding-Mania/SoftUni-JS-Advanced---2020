b23cd7de6b7c6ca8295141eab327a1c3fc26b395
CodingMania <vasilmitovv@gmail.com>
2020-01-29 10:45:07 +0200

    Solved task

--- JS Advanced/DOM-Exercise-Resources/01. Chat-Room/solution.js
+++ JS Advanced/DOM-Exercise-Resources/01. Chat-Room/solution.js
@@ -1,5 +1,24 @@
 function solve() {
-   //TODO...
+   let sendBotton = document.getElementById('send');
+
+   sendBotton.addEventListener('click', addContent);
+
+   function addContent() {
+
+      let content = document.getElementById('chat_input').value;
+
+      if (content.length > 0) {
+
+         let div = document.getElementById('chat_messages');
+
+         let messageDiv = document.createElement('div');
+         messageDiv.classList.add('message');
+         messageDiv.classList.add('my-message');
+         messageDiv.textContent = content;
+
+         div.appendChild(messageDiv);
+
+         document.getElementById('chat_input').value = '';
+      }
+   }
 }
-
-

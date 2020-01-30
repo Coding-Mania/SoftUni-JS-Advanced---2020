89f2e36af54f5a78ec0cf8cb6ee99a2c84cc0222
CodingMania <vasilmitovv@gmail.com>
2020-01-30 17:55:30 +0200

    Not completed

--- JS Advanced/DOM-Exercise-Resources/04. Numpad-Calculator/solution.js
+++ JS Advanced/DOM-Exercise-Resources/04. Numpad-Calculator/solution.js
@@ -1,4 +1,27 @@
 function solve() {
 
-    //TODO...
+    let key = document.querySelector('.keys');
+    let view = document.querySelector('#expressionOutput');
+    let outputView = document.querySelector('#resultOutput');
+    let clearButton = document.querySelector('.clear');
+
+    key.addEventListener('click', eventHandler);
+    clearButton.addEventListener('click', clearScreen);
+
+    function eventHandler(event){
+
+        console.log(event.target.value);
+        if(event.target.value !== '='){
+
+            view.innerHTML += event.target.value;
+        }
+        else{
+            
+        }    
+    }
+
+    function clearScreen(){
+        view.innerHTML ='';
+        outputView.innerHTML = '';
+    }
 }

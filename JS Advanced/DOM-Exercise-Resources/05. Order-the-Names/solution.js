0dd9d7038d2c7b072ed7b5628049206da350e427
CodingMania <vasilmitovv@gmail.com>
2020-01-31 09:59:17 +0200

    Completed solution

--- JS Advanced/DOM-Exercise-Resources/05. Order-the-Names/solution.js
+++ JS Advanced/DOM-Exercise-Resources/05. Order-the-Names/solution.js
@@ -1,4 +1,27 @@
 function solve() {
 
-    //TODO...
+    let button = document.querySelector('button');
+    let list = document.querySelectorAll('ol li');
+
+    console.log(list)
+
+    button.addEventListener('click', eventHandler);
+
+    function eventHandler() {
+        let name = document.querySelector('input').value;
+
+        if (name.length > 0) {
+
+            document.querySelector('input').value = '';
+
+            let index = name[0].toLocaleUpperCase().charCodeAt(0) - 65;
+
+            name = name[0].toLocaleUpperCase() + name.slice(1).toLocaleLowerCase();
+
+            let result = list[index].innerHTML.length > 0 ? list[index].innerHTML.split(', ') : [];
+
+            result.push(name);
+            list[index].innerHTML = result.join(', ');
+        }
+    }
 }

442a215e9ff2d7b0abddfa592a4e49c835d22e4c
CodingMania <vasilmitovv@gmail.com>
2020-01-31 10:52:52 +0200

    Completed solution

--- JS Advanced/DOM-Exercise-Resources/06. Table-Search-Engine/solution.js
+++ JS Advanced/DOM-Exercise-Resources/06. Table-Search-Engine/solution.js
@@ -1,3 +1,27 @@
 function solve() {
-   //TODO...
+
+   let button = document.querySelector('#searchBtn');
+   let table = document.querySelectorAll('tbody tr');
+
+   button.addEventListener('click', eventHandler);
+
+   function eventHandler() {
+
+      let searchValue = document.querySelector('#searchField').value.toLocaleLowerCase();
+      if (searchValue.trim().length > 0) {
+
+         for (const tr of table) {
+            tr.classList.remove('select');
+         }
+
+         document.querySelector('#searchField').value = '';
+
+         for (const tr of table) {
+
+            if (tr.innerHTML.toLocaleLowerCase().includes(searchValue)) {
+               tr.classList.toggle('select');
+            }
+         }
+      }
+   }
 }

4862a5f6ffb4dda7d70e5c33b34ec8f2c2dc3579
CodingMania <vasilmitovv@gmail.com>
2020-02-01 12:13:17 +0200

    Not completed

--- JS Advanced/DOM-Exercise-Resources/08. Furniture/solution.js
+++ JS Advanced/DOM-Exercise-Resources/08. Furniture/solution.js
@@ -1,4 +1,22 @@
 function solve() {
 
-  //TODO...
+  let buttons = document.getElementsByTagName('button');
+  let textarea = document.getElementsByTagName('textarea');
+
+  let inputArea = textarea[0];
+  let outputArea = textarea[1];
+
+
+  let generateButton = buttons[0];
+  let buyButton = buttons[1];
+
+  generateButton.addEventListener('click', eventHandler);
+
+  function eventHandler() {
+    let obj = inputArea.value;
+    inputArea.value = null;
+
+    console.log(obj);
+  }
+
 }

a9398aca36d57275e5c53cc5c8ddb45a399f1084
CodingMania <vasilmitovv@gmail.com>
2020-02-03 10:37:31 +0200

    Completed solution

--- JS Advanced/DOM-Manipulations-Exercise-Resources/02. Fill-Dropdown/dropdown.js
+++ JS Advanced/DOM-Manipulations-Exercise-Resources/02. Fill-Dropdown/dropdown.js
@@ -1,3 +1,16 @@
 function addItem() {
-    console.log('TODO:...');
+
+    let newItemText = document.querySelector('#newItemText');
+    let newItemValue = document.querySelector('#newItemValue');
+
+    let selectMenu = document.querySelector('#menu');
+
+    let option = document.createElement('option');
+    option.innerHTML = newItemText.value;
+    option.value = newItemValue.value;
+
+    newItemText.value = null;
+    newItemValue.value = null;
+
+    selectMenu.appendChild(option);
 }

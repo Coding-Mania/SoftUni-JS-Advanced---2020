9f7106efd4b2d48226458177976a306079b930dc
CodingMania <vasilmitovv@gmail.com>
2020-02-03 11:35:15 +0200

    Completed solution

--- JS Advanced/DOM-Manipulations-Exercise-Resources/04. Sections/script.js
+++ JS Advanced/DOM-Manipulations-Exercise-Resources/04. Sections/script.js
@@ -1,3 +1,22 @@
 function create(words) {
-   console.log('TODO:...');
+
+   let contentDiv = document.querySelector('#content');
+
+   for (const word of words) {
+
+      let div = document.createElement('div');
+      let p = document.createElement('p');
+      p.innerHTML = word;
+      p.style.display = 'none';
+
+      div.appendChild(p);
+
+      contentDiv.appendChild(div);
+   }
+
+   Array.from(contentDiv.children).forEach(div => {
+      div.addEventListener('click', () => {
+         div.firstChild.style.display = 'block';
+      })
+   })
 }

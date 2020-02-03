ff3f61e67a7ece28fffe392a7fcbaf0e4778843c
CodingMania <vasilmitovv@gmail.com>
2020-02-03 16:03:44 +0200

    Completed solution

--- JS Advanced/DOM-Manipulations-Exercise-Resources/06. Time-Converter/solution.js
+++ JS Advanced/DOM-Manipulations-Exercise-Resources/06. Time-Converter/solution.js
@@ -1,4 +1,44 @@
 function attachEventsListeners() {
 
-    console.log('TODO:...');
+    let main = document.querySelector('main');
+
+    main.addEventListener('click', eventHandler);
+
+    function eventHandler(event) {
+        if (event.target.type === 'button') {
+            let input = event.target.previousElementSibling;
+            let type = input.id;
+            let value = +input.value;
+
+            convertTime(type,value);
+        }
+    }
+
+    function convertTime(inputType, inputValue) {
+        if (inputType === 'days') {
+            let hours = inputValue * 24;
+            let minutes = hours * 60;
+            let seconds = minutes * 60;
+
+            setValues(inputValue, hours, minutes, seconds);
+        } else if (inputType === 'hours') {
+            let days = inputValue / 24;
+            return convertTime('days', days);
+        } else if (inputType === 'minutes') {
+            let hours = inputValue / 60;
+            return convertTime('hours', hours);
+        } else if (inputType === 'seconds') {
+            let minutes = inputValue / 60;
+            return convertTime('minutes', minutes);
+        }
+    }
+
+    function setValues(days, hours, minutes, seconds) {
+        let divs = main.querySelectorAll('main div input:nth-child(2)');
+
+       divs[0].value = days;
+       divs[1].value = hours;
+       divs[2].value = minutes;
+       divs[3].value = seconds;
+    }
 }

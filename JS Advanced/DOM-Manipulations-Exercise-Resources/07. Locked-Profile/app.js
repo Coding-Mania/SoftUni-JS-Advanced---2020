c249eaf31864cc3fe3b20d8fa6e0e2f4f9cfd558
CodingMania <vasilmitovv@gmail.com>
2020-02-03 16:41:38 +0200

    Completed solution

--- JS Advanced/DOM-Manipulations-Exercise-Resources/07. Locked-Profile/app.js
+++ JS Advanced/DOM-Manipulations-Exercise-Resources/07. Locked-Profile/app.js
@@ -1,3 +1,24 @@
 function lockedProfile() {
-    console.log('TODO...')
+
+    let main = document.querySelector('main');
+
+    main.addEventListener('click', eventHandler);
+
+    function eventHandler(event) {
+        if (event.target.type === 'submit') {
+            let div = event.target.parentElement;
+
+            let lock = div.querySelector('input');
+            let hiddenDivName = '#' + lock.name.split('Locked')[0] + 'HiddenFields';
+            let hiddenDiv = div.querySelector(hiddenDivName);
+
+            if (!lock.checked && event.target.textContent === 'Show more') {
+                event.target.textContent = 'Hide it';
+                hiddenDiv.style.display = 'block';
+            } else if (!lock.checked && event.target.textContent === 'Hide it') {
+                hiddenDiv.style.display = 'none';
+                event.target.textContent = 'Show more';
+            }
+        }
+    }
 }

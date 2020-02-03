e6ebff4fb35692b4715b1c10facbc6ed742e4979
CodingMania <vasilmitovv@gmail.com>
2020-02-03 16:56:06 +0200

    Completed solution

--- JS Advanced/DOM-Manipulations-Exercise-Resources/08. Encode-and-Decode-Messages/app.js
+++ JS Advanced/DOM-Manipulations-Exercise-Resources/08. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,34 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+
+    let buttons = document.querySelectorAll('button');
+
+    let encodeButton = buttons[0];
+    let decodeButton = buttons[1];
+
+    let textAreas = document.querySelectorAll('textarea');
+
+    let inputArea = textAreas[0];
+    let outputArea = textAreas[1];
+
+    encodeButton.addEventListener('click', encodeMessage);
+    decodeButton.addEventListener('click', decodeMessage);
+
+    function encodeMessage() {
+
+        let message = inputArea.value.split('').map((char) => {
+            return String.fromCharCode((char.charCodeAt(0) + 1));
+        }).join('');
+
+        inputArea.value = null;
+        outputArea.value = message;
+    }
+
+    function decodeMessage() {
+
+        let message = outputArea.value.split('').map((char) => {
+            return String.fromCharCode((char.charCodeAt(0) - 1));
+        }).join('');
+
+        outputArea.value = message;
+    }
 }

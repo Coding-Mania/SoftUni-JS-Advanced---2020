108be8e0a9bad27fcf31ba263d7b9e6bd4158cb8
CodingMania <vasilmitovv@gmail.com>
2020-02-05 17:25:52 +0200

    Completed solution

---
+++ JS Advanced/Classes/lengthLimit.js
@@ -0,0 +1,22 @@
+class Stringer {
+    constructor(string, length) {
+        this.innerString  = string;
+        this.innerLength = length;
+    }
+
+    increase(length) {
+        this.innerLength += length;
+    }
+
+    decrease(length) {
+        if (this.innerLength - length < 0) {
+            this.innerLength = 0;
+        } else {
+            this.innerLength -= length;
+        }
+    }
+
+    toString() {
+        return this.innerString.slice(0, this.innerLength) + (this.innerLength < this.innerString.length ? '...' : '');
+    }
+}

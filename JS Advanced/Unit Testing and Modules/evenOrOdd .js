0f2574b5ba81324bb26ff10beac42e562c2f7b32
CodingMania <vasilmitovv@gmail.com>
2020-02-10 13:29:57 +0200

    Initial commit

---
+++ JS Advanced/Unit Testing and Modules/evenOrOdd .js
@@ -0,0 +1,29 @@
+let assert = require('chai').assert;
+let isOddOrEven = require('./functionality');
+
+describe('isOddOrEven functionality', () => {
+    it('Should return undefined with non string input - boolean', () => {
+        let actual = isOddOrEven(true);
+        let expected = undefined;
+
+        assert.equal(expected, actual);
+    });
+    it('Should return undefined with non string input - number', () => {
+        let actual = isOddOrEven(5);
+        let expected = undefined;
+
+        assert.equal(expected, actual);
+    });
+    it('Should return even', () => {
+        let actual = isOddOrEven('test');
+        let expected = 'even';
+
+        assert.equal(expected, actual);
+    });
+    it('Should return odd', () => {
+        let actual = isOddOrEven('tests');
+        let expected = 'odd';
+
+        assert.equal(expected, actual);
+    });
+})

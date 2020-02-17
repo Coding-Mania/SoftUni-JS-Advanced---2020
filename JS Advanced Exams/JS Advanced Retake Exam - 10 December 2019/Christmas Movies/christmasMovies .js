01fc38a50e1a4736b10ad628f6dc7280ce990ce5
CodingMania <vasilmitovv@gmail.com>
2020-02-17 10:37:50 +0200

    Completed solution

--- JS Advanced Exams/JS Advanced Retake Exam - 10 December 2019/Christmas Movies/christmasMovies .js
+++ JS Advanced Exams/JS Advanced Retake Exam - 10 December 2019/Christmas Movies/christmasMovies .js
@@ -31,29 +31,93 @@ describe('ChristmasMovies functionality', () => {
             assert.throws(actual, expected);
         });
 
-        describe('discardMovie behaviour', () => {
-            it('Should throw Error with no existing movie', () => {
-                let actual = () => christmasMovies.discardMovie('test');
-                let expected = 'test is not at your collection!';
+        it('Should set only unique actors', () => {
+            let actual = christmasMovies.buyMovie('test', ['test', 'test1','test']);
+            let expected = 'You just got test to your collection in which test, test1 are taking part!';
 
-                assert.throws(actual, expected);
-            });
-            it('Should throw Error with no watched movie', () => {
-                christmasMovies.buyMovie('test', ['test', 'test1']);
-                let actual = () => christmasMovies.discardMovie('test');
-                let expected = 'test is not watched!';
+            assert.equal(actual, expected);
+        });
+    });
 
-                assert.throws(actual, expected);
-            });
-            it('Should work correct', () => {
-                christmasMovies.buyMovie('test', ['test', 'test1']);
-                christmasMovies.watchMovie('test');
+    describe('discardMovie behaviour', () => {
+        it('Should throw Error with no existing movie', () => {
+            let actual = () => christmasMovies.discardMovie('test');
+            let expected = 'test is not at your collection!';
 
-                let actual = christmasMovies.discardMovie('test');
-                let expected = 'You just threw away test!';
+            assert.throws(actual, expected);
+        });
+        it('Should throw Error with no watched movie', () => {
+            christmasMovies.buyMovie('test', ['test', 'test1']);
+            let actual = () => christmasMovies.discardMovie('test');
+            let expected = 'test is not watched!';
 
-                assert.equal(actual, expected);
-            });
+            assert.throws(actual, expected);
+        });
+        it('Should work correct', () => {
+            christmasMovies.buyMovie('test', ['test', 'test1']);
+            christmasMovies.watchMovie('test');
+
+            let actual = christmasMovies.discardMovie('test');
+            let expected = 'You just threw away test!';
+
+            assert.equal(actual, expected);
+        });
+    });
+
+    describe('watchMovie behaviour', () => {
+        it('Should throw Error with no existing movie', () => {
+            let actual = () => christmasMovies.watchMovie('test');
+            let expected = 'No such movie in your collection!';
+
+            assert.throws(actual, expected);
+        });
+        it('Should work correct', () => {
+            christmasMovies.buyMovie('test', ['test', 'test1']);
+            christmasMovies.watchMovie('test');
+            let actual = christmasMovies.watched['test'];
+            let expected = 1;
+
+            assert.equal(actual, expected);
+        });
+    });
+
+    describe('favoriteMovie behaviour', () => {
+        it('Should throw Error with no have favorite movie', () => {
+            let actual = () => christmasMovies.favouriteMovie();
+            let expected = 'You have not watched a movie yet this year!';
+
+            assert.throws(actual, expected);
+        });
+        it('Should return correct value', () => {
+            christmasMovies.buyMovie('test', ['test', 'test1']);
+            christmasMovies.buyMovie('test1', ['test1', 'test1']);
+            christmasMovies.watchMovie('test1');
+            christmasMovies.watchMovie('test');
+            christmasMovies.watchMovie('test');
+
+            let actual = christmasMovies.favouriteMovie();
+            let expected = 'Your favourite movie is test and you have watched it 2 times!';
+
+            assert.equal(actual, expected);
+        });
+    });
+
+    describe('mostStarredActor behaviour', () => {
+        it('Should throw Error with no watched movie', () => {
+            let actual = () => christmasMovies.mostStarredActor();
+            let expected = 'You have not watched a movie yet this year!';
+
+            assert.throws(actual, expected);
+        });
+        it('Should return correct value', () => {
+            christmasMovies.buyMovie('test', ['test', 'test1']);
+            christmasMovies.buyMovie('test1', ['test', 'test2']);
+            christmasMovies.buyMovie('test2', ['test', 'test3']);
+
+            let actual = christmasMovies.mostStarredActor();
+            let expected = 'The most starred actor is test and starred in 3 movies!';
+
+            assert.equal(actual, expected);
         });
     });
 });

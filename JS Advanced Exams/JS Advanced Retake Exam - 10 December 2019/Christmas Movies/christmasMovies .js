5769baaafc0d9a9b6d1960bb57a792c27b5b07ef
CodingMania <vasilmitovv@gmail.com>
2020-02-17 10:12:33 +0200

    Deleted repeating code

--- JS Advanced Exams/JS Advanced Retake Exam - 10 December 2019/Christmas Movies/christmasMovies .js
+++ JS Advanced Exams/JS Advanced Retake Exam - 10 December 2019/Christmas Movies/christmasMovies .js
@@ -55,57 +55,5 @@ describe('ChristmasMovies functionality', () => {
                 assert.equal(actual, expected);
             });
         });
-
-        describe('ChristmasMovies functionality', () => {
-            let christmasMovies;
-            beforeEach(() => {
-                christmasMovies = new ChristmasMovies();
-            })
-
-            describe('Ctor behaviour', () => {
-                it('Should set correct values', () => {
-                    let actual = {
-                        movieCollection: [],
-                        watched: {},
-                        actors: []
-                    }
-                    let expected = christmasMovies;
-                    assert.deepEqual(actual, expected);
-                });
-            });
-
-            describe('buyMovie behaviour', () => {
-                it('Should return correct value', () => {
-                    let actual = christmasMovies.buyMovie('test', ['test', 'test1']);
-                    let expected = 'You just got test to your collection in which test, test1 are taking part!';
-
-                    assert.equal(actual, expected);
-                });
-                it('Should throw Error with already own movie', () => {
-                    christmasMovies.buyMovie('test', ['test', 'test1']);
-                    let actual = () => christmasMovies.buyMovie('test', ['test', 'test1']);
-                    let expected = 'You already own test in your collection!';
-
-                    assert.throws(actual, expected);
-                });
-
-                describe('watchMovie behaviour', () => {
-                    it('Should throw Error with no existing movie', () => {
-                        let actual = () => christmasMovies.watchMovie('test');
-                        let expected = 'No such movie in your collection!';
-
-                        assert.throws(actual, expected);
-                    });
-                    it('Should work correct', () => {
-                        christmasMovies.buyMovie('test', ['test', 'test1']);
-                        christmasMovies.watchMovie('test');
-                        let actual = christmasMovies.watched['test'];
-                        let expected = 1;
-
-                        assert.equal(actual, expected);
-                    });
-                });
-            });
-        });
     });
 });

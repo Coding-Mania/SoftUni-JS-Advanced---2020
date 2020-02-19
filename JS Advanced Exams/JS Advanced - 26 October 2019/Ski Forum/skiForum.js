fed3c7e56b1112e148682294e4ba41a71dd05eb3
CodingMania <vasilmitovv@gmail.com>
2020-02-19 12:28:00 +0200

    Two test fail

---
+++ JS Advanced Exams/JS Advanced - 26 October 2019/Ski Forum/skiForum.js
@@ -0,0 +1,161 @@
+class Forum {
+    _users = [];
+    _questions = [];
+    _id = 1;
+
+    register(username, password, repeatPassword, email) {
+        if (username === '' || password === '' || repeatPassword === '' || email === '') {
+            throw new Error('Input can not be empty');
+        } else if (password !== repeatPassword) {
+            throw new Error('Passwords do not match');
+        }
+
+        let user = null;
+
+        this._users.forEach(e => {
+            if (e.username === username || e.email === email) {
+                user = e;
+            }
+        });
+
+        if (user) {
+            throw new Error('This user already exists!');
+        }
+
+        this._users.push({
+            username,
+            password,
+            email
+        });
+
+        return `${username} with ${email} was registered successfully!`;
+    }
+
+    login(username, password) {
+
+        let user = null;
+
+        this._users.forEach(e => {
+            if (e.username === username && e.password === password) {
+                user = e;
+            }
+        });
+
+        if (!user) {
+            throw new Error('There is no such user');
+        }
+
+        user.login = true;
+
+        return 'Hello! You have logged in successfully';
+    }
+
+    logout(username, password) {
+
+        let user = null;
+
+        this._users.forEach(e => {
+            if (e.username === username && e.password === password) {
+                user = e;
+            }
+        });
+
+        if (!user) {
+            throw new Error('There is no such user');
+        }
+
+        user.login = false;
+        return 'You have logged out successfully'
+    }
+
+    postQuestion(username, question) {
+        let user = null;
+
+        this._users.forEach(e => {
+            if (e.username === username) {
+                user = e;
+            }
+        });
+
+        if (!user || user.login === false) {
+            throw new Error('You should be logged in to post questions');
+        }
+
+        if (question === '') {
+            throw new Error('Invalid question');
+        }
+
+        this._questions.push({
+            id: this._id,
+            question,
+            by: username,
+            answers: []
+        });
+
+        this._id++;
+
+        return 'Your question has been posted successfully';
+    }
+
+    postAnswer(username, questionId, answer) {
+
+        let user = null;
+
+        this._users.forEach(e => {
+            if (e.username === username) {
+                user = e;
+            }
+        });
+
+        if (!user || user.login === false) {
+            throw new Error('You should be logged in to post answers');
+        }
+
+        if (answer === '') {
+            throw new Error('Invalid answer');
+        }
+
+        let question = null;
+
+        for (const id in this._questions) {
+            if (this._questions[id].id === questionId) {
+                question = this._questions[id];
+                break;
+            }
+        }
+
+        if (!question) {
+            throw new Error('There is no such question');
+        }
+
+        question.answers.push(`${username}: ${answer}`)
+        return 'Your answer has been posted successfully';
+    }
+
+    showQuestions() {
+        return this._questions.reduce((acc, q) => {
+            acc += `\nQuestion ${q.id} by ${q.by}: ${q.question}\n`;
+            acc += q.answers.reduce((acc2, a) => {
+                acc2 += `---${a}\n`
+                return acc2;
+            }, '').trim();
+            return acc;
+        }, '').trim();
+    }
+}
+
+let forum = new Forum();
+
+forum.register('Michael', '123', '123', 'michael@abv.bg');
+forum.register('Stoyan', '123ab7', '123ab7', 'some@gmail@.com');
+forum.login('Michael', '123');
+forum.login('Stoyan', '123ab7');
+
+forum.postQuestion('Michael', "Can I rent a snowboard from your shop?");
+forum.postAnswer('Stoyan', 1, "Yes, I have rented one last year.");
+forum.postQuestion('Stoyan', "How long are supposed to be the ski for my daughter?");
+forum.postAnswer('Michael', 2, "How old is she?");
+forum.postAnswer('Michael', 2, "Tell us how tall she is.");
+
+console.log(forum.showQuestions());
+

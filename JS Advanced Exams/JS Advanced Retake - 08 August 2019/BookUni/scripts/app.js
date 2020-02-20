03b132e6c2ba22e9c7b2c6d64a141c41c993d3d1
CodingMania <vasilmitovv@gmail.com>
2020-02-20 11:43:36 +0200

    Four test fail

---
+++ JS Advanced Exams/JS Advanced Retake - 08 August 2019/BookUni/scripts/app.js
@@ -0,0 +1,86 @@
+function solve() {
+
+    let addNewBookButton = document.querySelector('form button');
+    let inputs = document.querySelectorAll('form input');
+    let outputsDivs = document.querySelectorAll('#outputs .bookShelf');
+    let totalPrice = document.querySelectorAll('h1')[1];
+
+    let oldBooks = outputsDivs[0];
+    let newBooks = outputsDivs[1];
+
+    addNewBookButton.addEventListener('click', addNewBookEventHandler);
+
+    function addNewBookEventHandler(e) {
+        e.preventDefault();
+
+        let title = inputs[0].value;
+        let year = +inputs[1].value;
+        let price = +inputs[2].value;
+
+        if (title.length > 0 && year > -1 && price > -1) {
+
+            let div = createHTMLElement('div', { name: 'class', value: 'book' });
+            let p = createHTMLElement('p', null, { type: 'innerHTML', value: `${title} [${year}]` });
+            let buyButton = createHTMLElement('button', null, { type: 'innerHTML', value: `Buy it only for ${price.toFixed(2)} BGN` }, 'click', buyBookHandler);
+
+            div.appendChild(p);
+            div.appendChild(buyButton);
+
+            if (year < 2000) {
+                oldBooks.appendChild(div);
+            } else {
+                let oldSectionButton = createHTMLElement('button', null, { type: 'innerHTML', value: `Move to old section` }, 'click', oldSectionHandler);
+                div.appendChild(oldSectionButton);
+                newBooks.appendChild(div);
+            }
+        }
+
+    }
+
+    function buyBookHandler() {
+
+        let parent = this.parentNode.parentNode;
+
+        let price = +this.parentNode.querySelector('button').innerHTML.match(/\d+.?\d+/g);
+
+        let currentTotalPrice = totalPrice.innerHTML.match(/\d+.?\d+/g) || 0;
+
+        totalPrice.innerHTML = `Total Store Profit: ${(price + currentTotalPrice).toFixed(2)} BGN`;
+
+        parent.removeChild(this.parentNode);
+    }
+
+    function oldSectionHandler() {
+
+        let parentNode = this.parentNode;
+
+        let oldPrice = +parentNode.querySelector('button').innerHTML.match(/\d+.?\d+/g);
+
+        let price = oldPrice * 0.85;
+
+        parentNode.querySelector('button').innerHTML = `Buy it only for ${price.toFixed(2)} BGN`;
+
+        parentNode.removeChild(this.parentNode.querySelectorAll('button')[1]);
+
+        oldBooks.appendChild(parentNode);
+    }
+
+    function createHTMLElement(tagName, attribute, valuesInput, event, eventHandler) {
+        let element = document.createElement(tagName);
+
+        if (attribute) {
+            element.setAttribute(attribute.name, attribute.value);
+        }
+
+        if (valuesInput) {
+            element[valuesInput.type] = valuesInput.value;
+        }
+
+        if (event) {
+            element.addEventListener(event, eventHandler);
+        }
+
+
+        return element;
+    }
+}

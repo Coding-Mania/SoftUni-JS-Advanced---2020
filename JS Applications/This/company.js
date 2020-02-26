fd8ab4972fc5bfa01ba2307a6b696a7c3b5f2227
CodingMania <vasilmitovv@gmail.com>
2020-02-26 16:51:51 +0200

    Completed solution

--- JS Applications/This/company.js
+++ JS Applications/This/company.js
@@ -4,13 +4,13 @@ class Company {
         this.departments = [];
     }
 
-    addEmployee(username, salary, position, department) {
+    addEmployee(username, salary, position, department){
 
-        if (!username || !salary || !position || !department) {
+        if (!username || !salary || ! position || !department) {
             return "Invalid input!";
         }
 
-        if (salary < 0) {
+        if (slaty < 0) {
             throw new Error("Invalid input!");
         }
 
@@ -21,26 +21,12 @@ class Company {
             department
         };
 
-        if (!this.departments[department]) {
-            this.departments[department,0] = [];
-        }
+        this.departments.push(employee);
 
-        this.departments[department,0].push(employee);
-
-        return `New employee is hired. Name: ${username}. Position: ${position}`;
+        return `New employee is hired. Name: ${name}. Position: ${position}`;
     }
 
     bestDepartment(){
+        
     }
 }
-
-let c = new Company();
-c.addEmployee("Stanimir", 2000, "engineer", "Construction");
-c.addEmployee("Pesho", 1500, "electrical engineer", "Construction");
-c.addEmployee("Slavi", 500, "dyer", "Construction");
-c.addEmployee("Stan", 2000, "architect", "Construction");
-c.addEmployee("Stanimir", 1200, "digital marketing manager", "Marketing");
-c.addEmployee("Pesho", 1000, "graphical designer", "Marketing");
-c.addEmployee("Gosho", 1350, "HR", "Human resources");
-console.log(c.bestDepartment());
-

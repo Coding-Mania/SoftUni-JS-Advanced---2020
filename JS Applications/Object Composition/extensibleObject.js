e0c63204b7f8d0dfba206f11b67ff4c7b919f656
CodingMania <vasilmitovv@gmail.com>
2020-03-02 17:46:23 +0200

    Completed solution

--- JS Applications/Object Composition/extensibleObject.js
+++ JS Applications/Object Composition/extensibleObject.js
@@ -1,3 +1,19 @@
 function solve(){
     
+    let obj = {
+
+        extend: function(template){
+
+            Object.keys(template).forEach(key => {
+                if(typeof template[key] === 'function'){
+                    Object.getPrototypeOf(this)[key] = template[key];
+                }   else{
+                    this[key] = template[key];
+                }
+            });
+
+        }
+    };
+
+    return obj;
 }

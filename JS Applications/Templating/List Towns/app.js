c345daaf316a8045356d4687877cd750113b1f11
CodingMania <vasilmitovv@gmail.com>
2020-03-23 11:36:45 +0200

    Check for empty input

--- JS Applications/Templating/List Towns/app.js
+++ JS Applications/Templating/List Towns/app.js
@@ -4,19 +4,22 @@
 
     async function loadTowns(e) {
         e.preventDefault();
-        rootDiv.innerHTML = '';
-
         const townsInput = document.querySelector('#towns');
         const towns = townsInput.value.split(', ');
-        townsInput.value = '';
 
-        const templateSrc = await fetch('./town-template.hbs').then(r => r.text());
+        if (towns.length > 0 && towns[0] !== '') {
+            rootDiv.innerHTML = '';
 
-        const template = Handlebars.compile(templateSrc)
-        
-        const resultHTML = template({towns});
-        
-        rootDiv.innerHTML = resultHTML;
+            townsInput.value = '';
+
+            const templateSrc = await fetch('./town-template.hbs').then(r => r.text());
+
+            const template = Handlebars.compile(templateSrc)
+
+            const resultHTML = template({ towns });
+
+            rootDiv.innerHTML = resultHTML;
+        }
     }
 
     loadBtn.addEventListener('click', loadTowns);

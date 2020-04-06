5d8df3fd86abbb4cb4868c264557ac155039c61e
CodingMania <vasilmitovv@gmail.com>
2020-04-06 09:33:55 +0300

    Refactor home controller

--- JS Applications Exam practise/SoftWiki/controllers/home.js
+++ JS Applications Exam practise/SoftWiki/controllers/home.js
@@ -8,17 +8,14 @@ export default {
                 models.article.getAll().then(r => {
                     const articles = r.docs.map(d => { return { ...d.data(), id: d.id } });
                     ctx.articles = articles || {};
+
+                    ctx.articles.sort((a, b) => b.title.localeCompare(a.title));
+                    
                     ctx.jsArticles = articles.filter(a => a.category.toLowerCase() === 'javascript');
-                    ctx.jsArticles.sort((a, b) => b.title.localeCompare(a.title));
-
                     ctx.cSharpArticles = articles.filter(a => a.category.toLowerCase() === 'c#');
-                    ctx.cSharpArticles.sort((a, b) => b.title.localeCompare(a.title));
-
-                    ctx.javaArticles = articles.filter(a => a.category.toLowerCase() === 'java');
-                    ctx.javaArticles.sort((a, b) => b.title.localeCompare(a.title));
-
+                    ctx.javaArticles = articles.filter(a => a.category.toLowerCase() === 'java');               
                     ctx.pytonArticles = articles.filter(a => a.category.toLowerCase() === 'pyton');
-                    ctx.pytonArticles.sort((a, b) => b.title.localeCompare(a.title));
+                   
 
                     extend(ctx).
                         partial('../views/home/home.hbs');
